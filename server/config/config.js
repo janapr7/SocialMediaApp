541209a05890d15efb9c476a3fbefa14c8c6252a
Anak Agung Gde Bagus Janapriya <bgsjanapriya@gmail.com>
2023-08-16 22:03:22 +0800

    server init config

---
+++ server/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: null,
+    database: "database_development",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: null,
+    database: "database_development",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  }
+}
---
+++ server/index.js
@@ -0,0 +1,63 @@
+const dotenv = require("dotenv")
+dotenv.config()
+const express = require("express");
+const cors = require("cors");
+const { join } = require("path");
+const db = require("./models");
+
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+
+app.use(cors())
+app.use(express.json());
+
+//#region API ROUTES
+// Import routes
+// const { userRouter } = require("./routers")
+
+// Add routes
+// app.use('/api/users', userRouter)
+
+
+
+app.use((req, res, next) => {
+  if (req.path.includes("/api/")) {
+    res.status(404).send("Not found!");
+  } else {
+    next();
+  }
+});
+
+// error
+app.use((err, req, res, next) => {
+  if (req.path.includes("/api/")) {
+    console.error("Error : ", err.stack);
+    res.status(500).send("Error!");
+  } else {
+    next();
+  }
+});
+
+//#endregion
+
+//#region CLIENT
+// const clientPath = "../../client/build";
+// app.use(express.static(join(__dirname, clientPath)));
+// app.use(express.static(__dirname + '/public'));
+
+// // Serve the HTML page
+// app.get("*", (req, res) => {
+//   res.sendFile(join(__dirname, clientPath, "index.html"));
+// });
+
+//#endregion
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(`ERROR: ${err}`);
+  } else {
+    // db.sequelize.sync({alter: true})
+    console.log(`APP RUNNING at ${PORT} ✅`);
+  }
+});
